50dc780e55407876dcffb18fa4463803027fcace
Catherine Aguilera <catherineaguilera13@gmail.com>
2022-02-17 11:26:49 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Counter } from './features/counter/Counter';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+	return (
+		<div>
+			<Header />
+		</div>
+	);
+}
+
+export default App;
