07a967a4bde6d5f17ea67a32940396e0f21596ff
Catherine Aguilera <catherineaguilera13@gmail.com>
2022-02-18 13:06:42 -0400

    adding login page

--- src/App.js
+++ src/App.js
@@ -3,12 +3,21 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Detail from './components/Detail';
 //import browser router y envolver todo
 function App() {
 	return (
 		<div>
-			<Header />
-			<Home />
+			<Router>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/login" element={<Login />} />
+					<Route path="/detail" element={<Detail />} />
+				</Routes>
+			</Router>
 		</div>
 	);
 }
