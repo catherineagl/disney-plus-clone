6a2a75b73f7e696cbf198f5ac4658cdba7b6a145
Catherine Aguilera <catherineaguilera13@gmail.com>
2022-02-18 10:02:52 -0400

    add detail page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { Counter } from './features/counter/Counter';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
-
+//import browser router y envolver todo
 function App() {
 	return (
 		<div>
